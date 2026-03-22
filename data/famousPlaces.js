38666216e437116c095e623e267e778c86d09850
Piyumal-Bandaranayake <piyumalbandaranayake24790@gmail.com>
2026-03-22 20:51:15 +0530

    feat: Add famous places data, associated images, a debug script, and a destinations page.

--- data/famousPlaces.js
+++ data/famousPlaces.js
@@ -2,88 +2,88 @@ export const famousPlacesData = {
     colombo: {
         name: "Colombo",
         places: [
-            { name: "Galle Face Green", image: "/galle_face.png" },
-            { name: "Gangaramaya Temple", image: "/gangaramayatemple.JPG" },
-            { name: "Independence Square", image: "/indipendentsqure.jpg" },
-            { name: "Viharamahadevi Park", image: "/viharamahadevipark.jpg" },
-            { name: "Colombo National Museum", image: "/national-museum-colombo-sri-lanka.jpg" }
+            { name: "Galle Face Green", image: "/famous_places/galle_face.png" },
+            { name: "Gangaramaya Temple", image: "/famous_places/gangaramayatemple.JPG" },
+            { name: "Independence Square", image: "/famous_places/indipendentsqure.jpg" },
+            { name: "Viharamahadevi Park", image: "/famous_places/viharamahadevipark.jpg" },
+            { name: "Colombo National Museum", image: "/famous_places/national-museum-colombo-sri-lanka.jpg" }
         ]
     },
     gampaha: {
         name: "Gampaha",
         places: [
-            { name: "Kelaniya Raja Maha Vihara", image: "/kalaniyatemple.jpg" },
-            { name: "Negombo Beach", image: "/negombobeach.jpg" },
-            { name: "Muthurajawela Wetland", image: "/muthurajawelawetland.jpg" },
-            { name: "Guruge Nature Park", image: "/gurugenaturepark.JPG" }
+            { name: "Kelaniya Raja Maha Vihara", image: "/famous_places/kalaniyatemple.jpg" },
+            { name: "Negombo Beach", image: "/famous_places/negombobeach.jpg" },
+            { name: "Muthurajawela Wetland", image: "/famous_places/muthurajawelawetland.jpg" },
+            { name: "Guruge Nature Park", image: "/famous_places/gurugenaturepark.JPG" }
         ]
     },
     kalutara: {
         name: "Kalutara",
         places: [
-            { name: "Kalutara Bodhiya", image: "https://images.unsplash.com/photo-1624891113524-7eb3266e7465?q=80&w=800&auto=format&fit=crop" },
-            { name: "Richmond Castle", image: "https://images.unsplash.com/photo-1620589125139-3832c3246eb4?q=80&w=800&auto=format&fit=crop" },
-            { name: "Kalutara Beach", image: "https://images.unsplash.com/photo-1588614959060-4d144f28b207?q=80&w=800&auto=format&fit=crop" },
+            { name: "Kalutara Bodhiya", image: "/famous_places/kalutharbodhiya.jpg" },
+            { name: "Richmond Castle", image: "/famous_places/richmondCastle.jpg" },
+            { name: "Kalutara Beach", image: "/famous_places/kalutaraBeach.jpg" },
 
         ]
     },
     kandy: {
         name: "Kandy",
         places: [
-            { name: "Temple of the Sacred Tooth Relic", image: "/kandy_temple.png" },
-            { name: "Royal Botanical Gardens Peradeniya", image: "/peradeniya.jpg" },
-            { name: "Udawattekele Sanctuary", image: "/udawathakale.jpg" },
-            { name: "Knuckles View Point", image: "/knkulesviewpoint.jpg" }
+            { name: "Temple of the Sacred Tooth Relic", image: "/famous_places/kandy_temple.png" },
+            { name: "Royal Botanical Gardens Peradeniya", image: "/famous_places/peradeniya.jpg" },
+            { name: "Udawattekele Sanctuary", image: "/famous_places/udawathakale.jpg" },
+            { name: "Knuckles View Point", image: "/famous_places/knkulesviewpoint.jpg" }
         ]
     },
     matale: {
         name: "Matale",
         places: [
-            { name: "Sigiriya Rock Fortress", image: "/sigiriya.png" },
-            { name: "Dambulla Cave Temple", image: "/dambulla.png" },
-            { name: "Riverston", image: "/knkulesviewpoint.jpg" },
-            { name: "Nalanda Gedige", image: "/Nalandagedige.jpg" },
-            { name: "Sembuwatta Lake", image: "/sembuwatta.jpg" }
+            { name: "Sigiriya Rock Fortress", image: "/famous_places/sigiriya.png" },
+            { name: "Dambulla Cave Temple", image: "/famous_places/dambulla.png" },
+            { name: "Riverston", image: "/famous_places/knkulesviewpoint.jpg" },
+            { name: "Nalanda Gedige", image: "/famous_places/Nalandagedige.jpg" },
+            { name: "Sembuwatta Lake", image: "/famous_places/sembuwatta.jpg" }
         ]
     },
     "nuwara-eliya": {
         name: "Nuwara Eliya",
         places: [
-            { name: "Horton Plains National Park", image: "/hortonPlainsNationalPark.jpg" },
-            { name: "Worlds End", image: "/worldend.jpg" },
-            { name: "Gregory Lake", image: "/geogrylake.jpg" },
-            { name: "Hakgala Botanical Garden", image: "/hakgala.jpg" },
-            { name: "Pedro Tea Estate", image: "/pedrotea.jpg" }
+            { name: "Horton Plains National Park", image: "/famous_places/hortonPlainsNationalPark.jpg" },
+            { name: "Worlds End", image: "/famous_places/worldend.jpg" },
+            { name: "Gregory Lake", image: "/famous_places/geogrylake.jpg" },
+            { name: "Hakgala Botanical Garden", image: "/famous_places/hakgala.jpg" },
+            { name: "Pedro Tea Estate", image: "/famous_places/pedrotea.jpg" }
         ]
     },
     galle: {
         name: "Galle",
         places: [
-            { name: "Galle Fort", image: "/galle_fort.png" },
-            { name: "Unawatuna Beach", image: "https://images.unsplash.com/photo-1582234032543-9f170af3664d?q=80&w=800&auto=format&fit=crop" },
-            { name: "Jungle Beach", image: "https://images.unsplash.com/photo-1586861635167-e5223aadc9fe?q=80&w=800&auto=format&fit=crop" },
-            { name: "Japanese Peace Pagoda", image: "https://images.unsplash.com/photo-1552465011-b4e21bf6e79a?q=80&w=800&auto=format&fit=crop" },
-            { name: "Koggala Lake", image: "https://images.unsplash.com/photo-1571003123894-1f0594d2b5d9?q=80&w=800&auto=format&fit=crop" }
+            { name: "Galle Fort", image: "/famous_places/galle_fort.png" },
+            { name: "Unawatuna Beach", image: "/famous_places/unawatunaBeach.jpg" },
+            { name: "Jungle Beach", image: "/famous_places/jungleBeach.jpg" },
+            { name: "Japanese Peace Pagoda", image: "/famous_places/japanesePeacePagoda.jpg" },
+            { name: "Koggala Lake", image: "/famous_places/koggalaLake.jpg" }
         ]
     },
     matara: {
         name: "Matara",
         places: [
-            { name: "Polhena Beach", image: "https://images.unsplash.com/photo-1588820023611-37d45ddf0f62?q=80&w=800&auto=format&fit=crop" },
-            { name: "Dondra Head Lighthouse", image: "https://images.unsplash.com/photo-1623947477545-c8a4103a89e4?q=80&w=800&auto=format&fit=crop" },
-            { name: "Weherahena Temple", image: "https://images.unsplash.com/photo-1581416880097-40c21dc77a94?q=80&w=800&auto=format&fit=crop" },
-            { name: "Matara Fort", image: "https://images.unsplash.com/photo-1552465011-b4e21bf6e79a?q=80&w=800&auto=format&fit=crop" },
-            { name: "Hummanaya Blowhole", image: "https://images.unsplash.com/photo-1586861635167-e5223aadc9fe?q=80&w=800&auto=format&fit=crop" }
+            { name: "Polhena Beach", image: "/famous_places/polhenabeach.jpg" },
+            { name: "Dondra Head Lighthouse", image: "/famous_places/dondrahead.jpg" },
+            { name: "Weherahena Temple", image: "/famous_places/weherahenatemple.jpg" },
+            { name: "Matara Fort", image: "/famous_places/matarafort.jpg" },
+            { name: "Hummanaya Blowhole", image: "/famous_places/hummanayablowhole.jpg" }
         ]
     },
     hambantota: {
         name: "Hambantota",
         places: [
-            { name: "Yala National Park", image: "/yala_leopard.png" },
-            { name: "Bundala National Park", image: "https://images.unsplash.com/photo-1598460599187-200eed59fb96?q=80&w=800&auto=format&fit=crop" },
-            { name: "Mirijjawila Botanical Garden", image: "https://images.unsplash.com/photo-1533038590840-1cbea6bc1e28?q=80&w=800&auto=format&fit=crop" },
-            { name: "Hambantota Salt Pans", image: "https://images.unsplash.com/photo-1598460599187-200eed59fb96?q=80&w=800&auto=format&fit=crop" },
-            { name: "Dry Zone Botanical Garden", image: "https://images.unsplash.com/photo-1533038590840-1cbea6bc1e28?q=80&w=800&auto=format&fit=crop" }
+            { name: "Yala National Park", image: "/famous_places/yala_leopard.png" },
+            { name: "Bundala National Park", image: "/famous_places/bundalanationalpark.jpg" },
+            { name: "Mirijjawila Botanical Garden", image: "/famous_places/mirijjawilabotanicalgarden.jpg" },
+            { name: "Hambantota Salt Pans", image: "/famous_places/hambantotasaltpans.jpg" },
+
         ]
     },
     jaffna: {
@@ -160,7 +160,7 @@ export const famousPlacesData = {
         name: "Trincomalee",
         places: [
             { name: "Koneswaram Temple", image: "https://images.unsplash.com/photo-1586526154388-12dce0291fc2?q=80&w=800&auto=format&fit=crop" },
-            { name: "Nilaveli Beach", image: "/nilaveli_beach.png" },
+            { name: "Nilaveli Beach", image: "/famous_places/nilaveli_beach.png" },
             { name: "Pigeon Island National Park", image: "https://images.unsplash.com/photo-1586526154388-12dce0291fc2?q=80&w=800&auto=format&fit=crop" },
             { name: "Marble Beach", image: "https://images.unsplash.com/photo-1586526154388-12dce0291fc2?q=80&w=800&auto=format&fit=crop" },
             { name: "Fort Frederick", image: "https://images.unsplash.com/photo-1586526154388-12dce0291fc2?q=80&w=800&auto=format&fit=crop" }
@@ -169,11 +169,11 @@ export const famousPlacesData = {
     kurunegala: {
         name: "Kurunegala",
         places: [
-            { name: "Ridi Viharaya", image: "https://images.unsplash.com/photo-1570168007204-dfb528c6958f?q=80&w=800&auto=format&fit=crop" },
-            { name: "Kurunegala Lake", image: "https://images.unsplash.com/photo-1570168007204-dfb528c6958f?q=80&w=800&auto=format&fit=crop" },
-            { name: "Ethagala Rock", image: "https://images.unsplash.com/photo-1570168007204-dfb528c6958f?q=80&w=800&auto=format&fit=crop" },
-            { name: "Yapahuwa Rock Fortress", image: "/yapahuwa.png" },
-            { name: "Panduwasnuwara Kingdom", image: "https://images.unsplash.com/photo-1570168007204-dfb528c6958f?q=80&w=800&auto=format&fit=crop" }
+            { name: "Ridi Viharaya", image: "/famous_places/ridivihare.jpg" },
+            { name: "Kurunegala Lake", image: "/famous_places/kurunagalalake.jpg" },
+            { name: "Ethagala Rock", image: "/famous_places/ethagala.jpg" },
+            { name: "Yapahuwa Rock Fortress", image: "/famous_places/yapahuwa.png" },
+            { name: "Panduwasnuwara Kingdom", image: "/famous_places/paduwasnuwar.webp" }
         ]
     },
     puttalam: {
@@ -190,7 +190,7 @@ export const famousPlacesData = {
         name: "Anuradhapura",
         places: [
             { name: "Sri Maha Bodhi", image: "https://images.unsplash.com/photo-1533038590840-1cbea6bc1e28?q=80&w=800&auto=format&fit=crop" },
-            { name: "Ruwanwelisaya", image: "/ruwanwelisaya.png" },
+            { name: "Ruwanwelisaya", image: "/famous_places/ruwanwelisaya.png" },
             { name: "Jetavanaramaya", image: "https://images.unsplash.com/photo-1533038590840-1cbea6bc1e28?q=80&w=800&auto=format&fit=crop" },
             { name: "Abhayagiri Stupa", image: "https://images.unsplash.com/photo-1533038590840-1cbea6bc1e28?q=80&w=800&auto=format&fit=crop" },
             { name: "Mihintale", image: "https://images.unsplash.com/photo-1533038590840-1cbea6bc1e28?q=80&w=800&auto=format&fit=crop" }
@@ -199,7 +199,7 @@ export const famousPlacesData = {
     polonnaruwa: {
         name: "Polonnaruwa",
         places: [
-            { name: "Gal Vihara", image: "/polonnaruwa_ruins.png" },
+            { name: "Gal Vihara", image: "/famous_places/polonnaruwa_ruins.png" },
             { name: "Parakrama Samudra", image: "https://images.unsplash.com/photo-1632766320074-ceadea9ee104?q=80&w=800&auto=format&fit=crop" },
             { name: "Polonnaruwa Vatadage", image: "https://images.unsplash.com/photo-1632766320074-ceadea9ee104?q=80&w=800&auto=format&fit=crop" },
             { name: "Lankatilaka Temple", image: "https://images.unsplash.com/photo-1632766320074-ceadea9ee104?q=80&w=800&auto=format&fit=crop" },
@@ -209,17 +209,17 @@ export const famousPlacesData = {
     badulla: {
         name: "Badulla",
         places: [
-            { name: "Dunhinda Falls", image: "https://images.unsplash.com/photo-1610488665039-3d1226b5ad56?q=80&w=800&auto=format&fit=crop" },
-            { name: "Lipton's Seat", image: "https://images.unsplash.com/photo-1610488665039-3d1226b5ad56?q=80&w=800&auto=format&fit=crop" },
-            { name: "Nine Arches Bridge", image: "/nine_arches.png" },
-            { name: "Muthiyangana Temple", image: "https://images.unsplash.com/photo-1610488665039-3d1226b5ad56?q=80&w=800&auto=format&fit=crop" },
-            { name: "Diyaluma Falls", image: "https://images.unsplash.com/photo-1610488665039-3d1226b5ad56?q=80&w=800&auto=format&fit=crop" }
+            { name: "Dunhinda Falls", image: "/famous_places/dunhindafalls.jpg" },
+            { name: "Lipton's Seat", image: "/famous_places/liptonseat.jpg" },
+            { name: "Nine Arches Bridge", image: "/famous_places/nine_arches.png" },
+            { name: "Muthiyangana Temple", image: "/famous_places/muthiyanganatemple.jpg" },
+            { name: "Diyaluma Falls", image: "/famous_places/diyalumafalls.jpg" }
         ]
     },
     monaragala: {
         name: "Monaragala",
         places: [
-            { name: "Madulsima", image: "/wild_lake.png" },
+            { name: "Madulsima", image: "/famous_places/wild_lake.png" },
             { name: "Nil Diya Pokuna", image: "https://images.unsplash.com/photo-1584988294622-cff2775ae7da?q=80&w=800&auto=format&fit=crop" },
             { name: "Buduruwagala Temple", image: "https://images.unsplash.com/photo-1584988294622-cff2775ae7da?q=80&w=800&auto=format&fit=crop" },
             { name: "Yala Block 3", image: "https://images.unsplash.com/photo-1584988294622-cff2775ae7da?q=80&w=800&auto=format&fit=crop" },
@@ -229,21 +229,20 @@ export const famousPlacesData = {
     ratnapura: {
         name: "Ratnapura",
         places: [
-            { name: "Sinharaja Forest Reserve", image: "https://images.unsplash.com/photo-1581020087815-46fdba93df9e?q=80&w=800&auto=format&fit=crop" },
-            { name: "Adam's Peak (Sri Pada)", image: "/adam_peak.png" },
-            { name: "Bopath Ella", image: "https://images.unsplash.com/photo-1581020087815-46fdba93df9e?q=80&w=800&auto=format&fit=crop" },
-            { name: "Udawalawe National Park", image: "https://images.unsplash.com/photo-1581020087815-46fdba93df9e?q=80&w=800&auto=format&fit=crop" },
-            { name: "Gem Museum Ratnapura", image: "https://images.unsplash.com/photo-1581020087815-46fdba93df9e?q=80&w=800&auto=format&fit=crop" }
+            { name: "Sinharaja Forest Reserve", image: "/famous_places/sinharaja.jpg" },
+            { name: "Adam's Peak (Sri Pada)", image: "/famous_places/adam_peak.png" },
+            { name: "Bopath Ella", image: "/famous_places/bopathella.jpg" },
+            { name: "Udawalawe National Park", image: "/famous_places/udawalwa.jpg" },
+            { name: "Gem Museum Ratnapura", image: "/famous_places/Gem.jpg" }
         ]
     },
     kegalle: {
         name: "Kegalle",
         places: [
-            { name: "Pinnawala Elephant Orphanage", image: "https://images.unsplash.com/photo-1630489958043-41bb33318991?q=80&w=800&auto=format&fit=crop" },
-            { name: "Belilena Cave", image: "https://images.unsplash.com/photo-1630489958043-41bb33318991?q=80&w=800&auto=format&fit=crop" },
-            { name: "Kitulgala", image: "https://images.unsplash.com/photo-1630489958043-41bb33318991?q=80&w=800&auto=format&fit=crop" },
-            { name: "Ridi Viharaya", image: "https://images.unsplash.com/photo-1630489958043-41bb33318991?q=80&w=800&auto=format&fit=crop" },
-            { name: "Seethawaka Botanical Garden", image: "https://images.unsplash.com/photo-1630489958043-41bb33318991?q=80&w=800&auto=format&fit=crop" }
+            { name: "Pinnawala Elephant Orphanage", image: "/famous_places/pinnawala.jpg" },
+            { name: "Belilena Cave", image: "/famous_places/belilenacave.jpg" },
+            { name: "Kitulgala", image: "/famous_places/kitulgala.jpg" },
+
         ]
     }
 };
